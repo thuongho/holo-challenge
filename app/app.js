766a367c7a7deff9c96ab9c0ca0f311bc7fc0e4f
Thuong Ho <thuong.t.ho@gmail.com>
2016-02-23 13:20:39 -0800

    get displays items from api

--- app/app.js
+++ app/app.js
@@ -22,6 +22,42 @@
     });
 
     // SERVICES
+    holoApp.factory('RESTService', ['$http', '$q', function($http, $q) {
+
+        var get = function() {
+            var deferred = $q.defer(),
+                url = 'http://2-dot-crowdev-template.appspot.com/v1/tests/?callback=JSON_CALLBACK';
+
+            $http.get(url).success(function(data, status, header, config) {
+                console.log(data);
+                deferred.resolve(data);
+            }).error(function(data, status, header, config) {
+                console.log(status);
+                deferred.reject(status);
+            });
+
+            return deferred.promise;
+        };
+
+        var post = function() {
+
+        };
+
+        var put = function() {
+
+        };
+
+        var remove = function() {
+
+        };
+
+        return {
+            get: get,
+            post: post,
+            put: put,
+            delete: remove
+        };
+    }]);
 
     // CONTROLLERS
     holoApp.controller('MaxTimeController', ['$scope', function($scope) {
@@ -40,5 +76,15 @@
                 $scope.maxTime = Math.max.apply(null, timeArray);
             }
         };
+    }]);
+
+    holoApp.controller('RESTController', ['$scope', 'RESTService', function($scope, RESTService) {
+        $scope.loadData = function() {
+            RESTService.get().then(function(data) {
+                $scope.sampleObjects = data;
+            });
+        };
+
+        console.log('scope: ' + JSON.stringify($scope.sampleObjects));
     }]);
 })();
