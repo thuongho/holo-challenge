634d3a55041aa0d4f2d195e9f0663afc85e9f157
Thuong Ho <thuong.t.ho@gmail.com>
2016-03-03 01:17:43 -0800

    separate services to its own file

---
+++ app/services.js
@@ -0,0 +1,90 @@
+(function(){
+    'use strict'
+
+    angular.module('holoApp').factory('RESTService', ['$http', '$q', function($http, $q) {
+
+        //$http.get('/someUrl', config).then(successCallback, errorCallback);
+        var get = function() {
+            var deferred = $q.defer(),
+                url = 'http://2-dot-crowdev-template.appspot.com/v1/tests/?callback=JSON_CALLBACK';
+
+            $http.get(url).success(function(data, status, header, config) {
+                //console.log(data);
+                deferred.resolve(data);
+            }).error(function(data, status, header, config) {
+                console.log(status);
+                deferred.reject(status);
+            });
+
+            return deferred.promise;
+        };
+
+        var post = function(pageObject) {
+            var newObject = {name: pageObject.name, value: pageObject.value};
+            var deferred = $q.defer();
+            var url = 'http://2-dot-crowdev-template.appspot.com/v1/tests/?callback=JSON_CALLBACK';
+            var header = {'Content-Type': 'application/x-www-form-urlencoded'};
+
+            //$http.post('/someUrl', data, config).then(successCallback, errorCallback);
+            $http.post(url, newObject, header).then(function(response) {
+                if (response.data.success) {
+                    console.log(response);
+                    deferred.resolve(true);
+                } else {
+                    deferred.resolve(false);
+                }
+            });
+            return deferred.promise;
+        };
+
+        var put = function(pageObject) {
+            //var updateObject = {name: pageObject.name, value: pageObject.value};
+            var updateObjectName = '&name=' + pageObject.name.replace(/ /g, '%20');
+            var updateObjectValue = '&value=' + pageObject.value.replace(/ /g, '%20');
+            var url = 'http://2-dot-crowdev-template.appspot.com/v1/tests/'
+                + pageObject.id
+                + '?callback=JSON_CALLBACK'
+                + updateObjectName
+                + updateObjectValue;
+
+            console.log(url);
+
+            $http.jsonp(url)
+                .success(function(data) {
+                    console.log(data + " updated.");
+                })
+                .error(function(data, status, header, config) {
+                    console.log('data: ' + data);
+                    console.log('status: ' + status);
+                    console.log('header: ' + header);
+                    console.log('config: ' + config);
+                });
+            //$http.put(url + updateObject)
+            //    .success(function(data, status, headers) {
+            //        console.log(data + " updated.");
+            //    })
+            //    .error(function(data, status, header, config) {
+            //        console.log(status);
+            //    });
+        };
+
+        var remove = function(objectID) {
+            //delete: { method: 'DELETE', params: {id: '@id'} }
+            var url = 'http://2-dot-crowdev-template.appspot.com/v1/tests/' + objectID.id + '?callback=JSON_CALLBACK';
+            $http.delete(url)
+                .success(function(data, status) {
+                    console.log(JSON.stringify(data) + " deleted.");
+                })
+                .error(function(data, status) {
+                    console.log(status);
+                });
+        };
+
+        return {
+            get: get,
+            post: post,
+            put: put,
+            delete: remove
+        };
+    }]);
+})();
