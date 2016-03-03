ef4bf53653420d35af4c54869287cd9a97d69d11
Thuong Ho <thuong.t.ho@gmail.com>
2016-03-03 04:51:22 -0800

    update name

--- app/pages/max_time/maxTimeController.js
+++ app/pages/max_time/maxTimeController.js
@@ -1,18 +1,20 @@
-(function(){
+(function () {
     'use strict';
 
-    angular.module('holoApp').controller('MaxTimeController', ['$scope', function($scope) {
+    angular.module('holoApp').controller('MaxTimeController', ['$scope', function ($scope) {
         $scope.title = "Max Time";
         $scope.maxTime;
 
-        $scope.getMaxTime = function() {
+        $scope.getMaxTime = function () {
             if ($scope.timeArray) {
                 var timeArray = $scope.timeArray;
 
                 // convert string to array of floats
                 timeArray = timeArray.replace(/[\[\]\{\}]/g, '').replace(/time:/g, '');
                 timeArray = timeArray.split(', ');
-                timeArray.map(function (num) { num = parseFloat(num); });
+                timeArray.map(function (num) {
+                    num = parseFloat(num);
+                });
 
                 $scope.maxTime = Math.max.apply(null, timeArray);
             }
